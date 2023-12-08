3430639fd439e2ff65b9475ff6bd2c97185460c6
lesliemw <l.marie1598@gmail.com>
2023-12-08 19:13:57 +0000

    edit files

--- src/ui/HoverCard.jsx
+++ src/ui/HoverCard.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { IoChevronDownCircleOutline, IoPlayCircleSharp } from "react-icons/io5";
 import { CiCirclePlus } from "react-icons/ci";
-import { BsHandThumbsUp } from "react-icons/bs";
+import { GoThumbsup } from "react-icons/go";
 
 const StyledHoverCard = styled.div`
   height: 40%;
@@ -14,8 +14,31 @@ const StyledHoverCard = styled.div`
   }
   button {
     background-color: transparent;
-    color: #fff;
+    color: var(--color-dark-white);
     border: none;
+    padding-right: 1rem;
+  }
+
+  button:hover {
+    color: #fff;
+  }
+  .buttons {
+    display: flex;
+    margin: 1rem;
+  }
+
+  .play {
+    color: #fff;
+  }
+  .play:hover {
+    color: var(--color-dark-white);
+  }
+  .chevron {
+    margin-left: 85px;
+  }
+
+  .information {
+    margin: 2rem;
   }
 `;
 
@@ -25,30 +48,32 @@ function HoverCard() {
       <div>
         <img alt="" src="../../public/uncharted-poster.jpeg" />
       </div>
-      <div>
+      <div className="buttons">
         <button>
           <span>
-            <IoPlayCircleSharp size={40} />
+            <IoPlayCircleSharp className="play" size={40} />
           </span>
         </button>
         <button>
           <span>
-            <CiCirclePlus size={40} />
+            <CiCirclePlus className="add" size={40} />
           </span>
         </button>
         <button>
           <span>
-            <BsHandThumbsUp size={37} />
+            <GoThumbsup className="thumb" size={37} />
           </span>
         </button>
         <button>
           <span>
-            <IoChevronDownCircleOutline size={40} />
+            <IoChevronDownCircleOutline className="chevron" size={40} />
           </span>
         </button>
       </div>
-      <div>Details</div>
-      <div>Genre</div>
+      <div className="information">
+        <div>Details</div>
+        <div>Genre</div>
+      </div>
     </StyledHoverCard>
   );
 }
